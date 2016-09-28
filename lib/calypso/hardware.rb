fac93d937d8e666c61a51c36359e8295830abacb
Michel Megens <dev@bietje.net>
2016-09-28 12:48:50 +0200

    calypso: add wrappers for hardware and tests
    Add wrapper classes for tests and hardware.
    
    Signed-off-by: Michel Megens <dev@bietje.net>

---
+++ lib/calypso/hardware.rb
@@ -0,0 +1,30 @@
+#
+#   Calypso module - Unit test hardware
+#   Copyright (C) 2016  Michel Megens <dev@bietje.net>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+module Calypso
+  class Hardware
+    attr_reader :id, :name, :cpu
+
+    def initialize(id, name, cpu)
+      @id = id
+      @name = name
+      @cpu = cpu
+    end
+  end
+end
+
